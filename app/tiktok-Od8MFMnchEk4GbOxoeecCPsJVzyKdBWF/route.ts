40b535088c0c8fa88714225c71089db5ce397044
ahmadrasheed22 <ahmad22rasheed22@gmail.com>
2026-04-14 22:17:04 +0500

    del-old/tiktokverification

--- app/tiktok-Od8MFMnchEk4GbOxoeecCPsJVzyKdBWF/route.ts
+++ app/tiktok-Od8MFMnchEk4GbOxoeecCPsJVzyKdBWF/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  return new Response('AwGqO9c0BUn3Cbi7wVRcBaXj9o0M4DdG', {
+  return new Response('Od8MFMnchEk4GbOxoeecCPsJVzyKdBWF', {
     headers: {
       'Content-Type': 'text/html',
       'Cache-Control': 'no-cache',
