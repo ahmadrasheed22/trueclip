2ef55476d8dbc18630e3fcd59af04c60ac387a99
ahmadrasheed22 <ahmad22rasheed22@gmail.com>
2026-04-14 21:33:30 +0500

    Fix TikTok verification with route handler

---
+++ app/tiktok-AwGqO9c0BUn3Cbi7wVRcBaXj9o0M4DdG/route.ts
@@ -0,0 +1,8 @@
+export async function GET() {
+  return new Response('AwGqO9c0BUn3Cbi7wVRcBaXj9o0M4DdG', {
+    headers: {
+      'Content-Type': 'text/html',
+      'Cache-Control': 'no-cache',
+    },
+  });
+}
